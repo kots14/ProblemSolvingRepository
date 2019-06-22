0e304dfe859aafcdac35a937de2c4b1870325d23
Koustav Ghosh <koustavg@amazon.com>
2019-06-22 21:57:44 +0530

    adding recursion problem solution

--- Recursion/AllPossiblePathsFromTopLeftToBottomRight.java
+++ Recursion/AllPossiblePathsFromTopLeftToBottomRight.java
@@ -4,7 +4,14 @@ import java.util.*;
 // Problem : https://www.geeksforgeeks.org/count-possible-paths-top-left-bottom-right-nxm-matrix/
 
 public class AllPossiblePathsFromTopLeftToBottomRight {
+    static int countPossiblePaths(int n, int m) {
+        //base case
+        if(m == 1 || n == 1)
+            return 1;
+        //constraint : from each cell you can either move only to right or down
+        return countPossiblePaths(n - 1, m) + countPossiblePaths(n, m - 1);
+    }
     public static void main(String[] args) {
-
+        System.out.println(countPossiblePaths(3, 3));
     }
 }
