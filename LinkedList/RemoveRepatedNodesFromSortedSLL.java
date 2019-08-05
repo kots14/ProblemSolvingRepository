86fa1b56b9ec055a8a294ae2b49b025090fba2eb
Koustav Ghosh <koustavg@amazon.com>
2019-08-06 00:20:10 +0530

    adding modified linked list program

--- LinkedList/RemoveRepatedNodesFromSortedSLL.java
+++ LinkedList/RemoveRepatedNodesFromSortedSLL.java
@@ -1,6 +1,3 @@
-import javhead.io.*;
-import javhead.util.*;
-
 // Given a sorted linked list, delete all nodes that have duplicate numbers, 
 // leaving only distinct numbers from the original list.
 
