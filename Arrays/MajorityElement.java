935d58fc7ff7ca5794a93e3c589d1dfb870ff4c3
Koustav Ghosh <koustavg@amazon.com>
2019-08-22 01:07:28 +0530

    adding majority element

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+//https://www.geeksforgeeks.org/majority-element/
+public class MajorityElement {
+	
+	//Time complexity : O(n)
+	//Space complexity : O(n)
+	private static int solve(int[] a) {
+		int output = 0;
+		HashMap<Integer, Integer> h = new HashMap<>();
+		HashSet<Integer> set = new HashSet<>();
+		for(int i = 0; i < a.length; i++) {
+			h.put(a[i], h.getOrDefault(a[i], 0) + 1);
+		}
+		int count = Integer.MIN_VALUE;
+		for(Map.Entry<Integer, Integer> me : h.entrySet()) {
+			if(count < me.getValue()) {
+				count = me.getValue();
+				output = me.getKey();
+				set.clear();
+				set.add(me.getKey());
+			} else if(count == me.getValue())
+				set.add(me.getKey());
+				
+		}
+		if(set.size() > 1)
+			return -1;
+		return output;
+	}
+	public static void main(String[] args) {
+		int a[] = {1, 3, 3, 3,  1, 1, 1, 2}; 
+		System.out.println(solve(a));
+	}
+
+}
