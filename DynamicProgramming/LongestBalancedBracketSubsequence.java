3826f5339f73aa8b1c9aff2b5f9874300b3cda53
Koustav Ghosh <koustavg@amazon.com>
2019-09-20 11:35:40 +0530

    adding new problem

--- DynamicProgramming/LongestBalancedBracketSubsequence.java
+++ DynamicProgramming/LongestBalancedBracketSubsequence.java
@@ -1,6 +1,35 @@
 // https://www.geeksforgeeks.org/length-longest-balanced-subsequence/
 public class LongestBalancedBracketSubsequence {
+	static int maxLength(String s, int n) { 
+	    int dp[][] = new int[n][n]; 
+	      
+	  
+	    // Considering all balanced substrings  
+	    // of length 2 
+	    for (int i = 0; i < n - 1; i++) 
+	        if (s.charAt(i) == '(' &&   
+	                s.charAt(i + 1)    == ')') 
+	            dp[i][i + 1] = 2; 
+	  
+	    // Considering all other substrings 
+	    for (int l = 2; l < n; l++) { 
+	        for (int i = 0, j = l; j < n; i++, j++) { 
+	            if (s.charAt(i) == '(' &&  
+	                      s.charAt(j) == ')') 
+	                dp[i][j] = 2 + dp[i + 1][j - 1]; 
+	  
+	            for (int k = i; k < j; k++)  
+	                dp[i][j] = Math.max(dp[i][j], 
+	                        dp[i][k] + dp[k + 1][j]);      
+	                              
+	        } 
+	    } 
+	  
+	    return dp[0][n - 1]; 
+	}
     public static void main(String[] args) {
-        System.out.println("hello");
+    	String s = "()(((((()"; 
+        int n = s.length() ; 
+        System.out.println(maxLength(s, n));
     }
 }
