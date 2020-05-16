e76c10f53c83a7d69a47cc79ad7f4e12d391515a
Koustav Ghosh <koustavg@amazon.com>
2020-05-16 19:38:00 +0530

    adding recursion problems

--- Recursion/SubsequenceOfString.java
+++ Recursion/SubsequenceOfString.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Problem : https://www.geeksforgeeks.org/print-subsequences-string/
 public class SubsequenceOfString {
 
+	private static void getSubsequences(String string, String temp, List<String> result) {
+		if(string.length() == 0) {
+			result.add(temp.toString());
+			return;
+		}
+		getSubsequences(string.substring(1), temp + string.charAt(0), result);
+		getSubsequences(string.substring(1), temp, result);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> result = new ArrayList<>();
+		getSubsequences("abc", "", result);
+		result.stream().forEach(s -> System.out.println(s));
 
 	}
 
